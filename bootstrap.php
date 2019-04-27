926a6c368edb120d891122f3272898a341ac645b
JohnLui <wenhanlv@gmail.com>
2019-04-27 18:12:10 +0800

    make looks better

---
+++ bootstrap.php
@@ -0,0 +1,78 @@
+<?php
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+$dotenv = Dotenv\Dotenv::create(BASE_PATH);
+$dotenv->load();
+
+\Model\Model::$config = [
+  'driver'    => 'mysql',
+  'host'      => env('DB_HOST', 'localhost'),
+  'port'      => env('DB_PORT', '3306'),
+  'database'  => env('DB_DATABASE', 'database'),
+  'username'  => env('DB_USERNAME', 'root'),
+  'password'  => env('DB_PASSWORD', 'password'),
+  'charset'   => 'utf8mb4',
+  'collation' => 'utf8mb4_unicode_ci',
+  'prefix'    => '',
+];
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
+use GraphQL\GraphQL;
+
+try {
+    $queryType = new ObjectType([
+        'name' => 'Query',
+        'fields' => [
+            'echo' => [
+                'type' => Type::string(),
+                'args' => [
+                    'message' => ['type' => Type::string()],
+                ],
+                'resolve' => function ($root, $args) {
+                    return $root['prefix'] . $args['message'];
+                }
+            ],
+        ],
+    ]);
+    $mutationType = new ObjectType([
+        'name' => 'Calc',
+        'fields' => [
+            'sum' => [
+                'type' => Type::int(),
+                'args' => [
+                    'x' => ['type' => Type::int()],
+                    'y' => ['type' => Type::int()],
+                ],
+                'resolve' => function ($root, $args) {
+                    return $args['x'] + $args['y'];
+                },
+            ],
+        ],
+    ]);
+    // See docs on schema options:
+    // http://webonyx.github.io/graphql-php/type-system/schema/#configuration-options
+    $schema = new Schema([
+        'query' => $queryType,
+        'mutation' => $mutationType,
+    ]);
+    $rawInput = file_get_contents('php://input');
+    $input = json_decode($rawInput, true);
+    $query = $input['query'];
+    $variableValues = isset($input['variables']) ? $input['variables'] : null;
+    $rootValue = ['prefix' => 'You said: '];
+    $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+    $output = $result->toArray();
+} catch (\Exception $e) {
+    $output = [
+        'error' => [
+            'message' => $e->getMessage()
+        ]
+    ];
+}
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($output);
--- public/graphql.php
+++ public/graphql.php
@@ -1,79 +1,11 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
 
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+// PUBLIC_PATH
+define('PUBLIC_PATH', __DIR__);
+// BASE_PATH
+define('BASE_PATH', dirname(PUBLIC_PATH));
 
-$dotenv = Dotenv\Dotenv::create(dirname(__DIR__));
-$dotenv->load();
-
-\Model\Model::$config = [
-  'driver'    => 'mysql',
-  'host'      => env('DB_HOST', 'localhost'),
-  'port'      => env('DB_PORT', '3306'),
-  'database'  => env('DB_DATABASE', 'database'),
-  'username'  => env('DB_USERNAME', 'root'),
-  'password'  => env('DB_PASSWORD', 'password'),
-  'charset'   => 'utf8mb4',
-  'collation' => 'utf8mb4_unicode_ci',
-  'prefix'    => '',
-];
-
-use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\Type;
-use GraphQL\Type\Schema;
-use GraphQL\GraphQL;
-
-try {
-    $queryType = new ObjectType([
-        'name' => 'Query',
-        'fields' => [
-            'echo' => [
-                'type' => Type::string(),
-                'args' => [
-                    'message' => ['type' => Type::string()],
-                ],
-                'resolve' => function ($root, $args) {
-                    return $root['prefix'] . $args['message'];
-                }
-            ],
-        ],
-    ]);
-    $mutationType = new ObjectType([
-        'name' => 'Calc',
-        'fields' => [
-            'sum' => [
-                'type' => Type::int(),
-                'args' => [
-                    'x' => ['type' => Type::int()],
-                    'y' => ['type' => Type::int()],
-                ],
-                'resolve' => function ($root, $args) {
-                    return $args['x'] + $args['y'];
-                },
-            ],
-        ],
-    ]);
-    // See docs on schema options:
-    // http://webonyx.github.io/graphql-php/type-system/schema/#configuration-options
-    $schema = new Schema([
-        'query' => $queryType,
-        'mutation' => $mutationType,
-    ]);
-    $rawInput = file_get_contents('php://input');
-    $input = json_decode($rawInput, true);
-    $query = $input['query'];
-    $variableValues = isset($input['variables']) ? $input['variables'] : null;
-    $rootValue = ['prefix' => 'You said: '];
-    $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
-    $output = $result->toArray();
-} catch (\Exception $e) {
-    $output = [
-        'error' => [
-            'message' => $e->getMessage()
-        ]
-    ];
-}
-header('Content-Type: application/json; charset=UTF-8');
-echo json_encode($output);
+// Autoload
+require BASE_PATH.'/vendor/autoload.php';
+// Bootstrap
+require BASE_PATH.'/bootstrap.php';
