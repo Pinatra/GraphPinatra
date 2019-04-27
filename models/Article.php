96b343242a10915884fce8354f8d91665beb91ed
JohnLui <wenhanlv@gmail.com>
2019-04-27 17:10:51 +0800

    GraohQL hello world!

---
+++ models/Article.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Model;
+
+class Article extends \Pinatra\Model\Model
+{
+}
---
+++ models/Model.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Model;
+
+use Illuminate\Database\Capsule\Manager;
+
+class Model extends \Illuminate\Database\Eloquent\Model
+{
+  public static $config = [];
+  private static $fired = false;
+  public function __construct()
+  {
+    if (!self::$fired) {
+      $capsule = new Manager;
+      $capsule->addConnection(self::$config);
+      $capsule->setAsGlobal();
+      $capsule->bootEloquent();
+      self::$fired = true;
+    }
+  }
+}
